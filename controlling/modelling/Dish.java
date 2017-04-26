8ed642551035b74e1930567421445ad3f4a812b4
fearef <fedor.arefyev@yahoo.com>
2017-04-26 15:11:38 +0200

    Add files via upload

---
+++ controlling/modelling/Dish.java
@@ -0,0 +1,12 @@
+package controlling.modelling;
+
+public class Dish {
+	private String name;
+	private float price;
+	private int size;
+
+	public Dish() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
