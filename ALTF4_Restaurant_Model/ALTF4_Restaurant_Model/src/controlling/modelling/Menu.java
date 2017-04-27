5c135b73aa2f5d67494a74196c4bd21997bc27ad
L0rs <l.stockum@hotmail.de>
2017-04-27 12:42:41 +0200

    Projekt richtig hinzugefügt

---
+++ ALTF4_Restaurant_Model/ALTF4_Restaurant_Model/src/controlling/modelling/Menu.java
@@ -0,0 +1,13 @@
+package controlling.modelling;
+import java.util.ArrayList;
+//import java.util.Date;
+
+public class Menu extends Submenu{
+	
+	private ArrayList <Submenu> menu_list;
+
+	public Menu() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ALTF4_Restaurant_Model/ALTF4_Restaurant_Model/src/controlling/modelling/Submenu.java
@@ -0,0 +1,16 @@
+package controlling.modelling;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Submenu {
+	private String name;
+	private String description;
+	private Date current_date;
+	private ArrayList <Dish> dishes;
+
+	public Submenu() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
