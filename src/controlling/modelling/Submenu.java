ecca4340590c2ffe2702bb7a3baabc215f8731c2
fearef <fedor.arefyev@yahoo.com>
2017-05-03 13:31:54 +0200

    Vervollstandigung von modelling, TableViewer

--- src/controlling/modelling/Submenu.java
+++ src/controlling/modelling/Submenu.java
@@ -13,4 +13,44 @@ public class Submenu {
 		// TODO Auto-generated constructor stub
 	}
 
+	public Submenu(String name, String description, Date current_date, ObservableList<Dish> dishes) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.current_date = current_date;
+		this.dishes = dishes;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getCurrent_date() {
+		return current_date;
+	}
+
+	public void setCurrent_date(Date current_date) {
+		this.current_date = current_date;
+	}
+
+	public ObservableList<Dish> getDishes() {
+		return dishes;
+	}
+
+	public void setDishes(ObservableList<Dish> dishes) {
+		this.dishes = dishes;
+	}
+
 }
